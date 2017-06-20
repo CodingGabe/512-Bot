ac760e53a2a6b6af4c35a6e5e15bfc3a5b7e9284
Gabe Amaya <GabeAmaya2@gmail.com>
2017-06-19 21:57:28 -0500

    deploying first version of app to Heroku

--- bot.js
+++ bot.js
@@ -1,6 +1,26 @@
 console.log('The bot is firing up...GO!')
 
 var Twit = require('twit');
-var Config = require('./config');
+var config = require('./config');
 
-var T = new Twit(Config);
+var T = new Twit(config);
+
+// grab user for a stream
+var stream = T.stream('user');
+// if someone follows me
+stream.on('follow', followed);
+
+// function to post a tweet when called 
+function tweetIt() {
+  var tweet = {
+    status: 'Check us out every Wednesday night at 7pm CST. #512goons'
+  }
+  T.post('statuses/update', tweet, tweeted);
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log('You done goofed');
+    } else {
+      console.log('Tweet posted!');
+    }
+  }
+}
