0987345d1035f9fe2e292fc6b92c3d6b9789431d
Gabe Amaya <csstrickster2@gmail.com>
2017-07-12 14:22:40 -0500

    fixed an error to which a variable was undefined thus preventing the bot from tweeting

--- bot.js
+++ bot.js
@@ -6,19 +6,19 @@ var config = require('./config');
 var T = new Twit(config);
 
 // grab user for a stream
-var stream = T.stream('user');
+// var stream = T.stream('user');
 // if someone follows me
-stream.on('follow', followed);
+// stream.on('follow', followed);
 
 tweetIt();
-setInterval(tweetIt, 1000*60*60*168) // Set to tweet once a week when I stream
+setInterval(tweetIt, 1000*60*60*24) // Set to tweet once a week when I stream
 
 // function to post a tweet when called
 function tweetIt() {
   var tweet = {
     status: 'Streaming live now! Check us out https://mixer.com/GABEthaGOON #512goons #SupportSmallStreamers'
   }
-  T.post('statuses/update', tweet, tweeted);
+  T.post('statuses/update', tweet, tweeted); // twitter api call to post functions 
   function tweeted(err, data, response) {
     if (err) {
       console.log('You done goofed');
